54477ac091b8a13d8ca753fb47b56d008fc6b25c
atborg <33327789+atborg@users.noreply.github.com>
2023-02-17 13:32:07 -0700

    env vars fix

--- utils/getQueryString.ts
+++ utils/getQueryString.ts
@@ -1,7 +1,7 @@
 export const getQueryString = (corpus: string, query: string) => {
   const queryString =
     (process.env.ASKME_API as string) +
-    "?" +
+    "/question?" +
     new URLSearchParams({
       domain: corpus,
       question: query,
