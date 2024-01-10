ff5640690048333535fa12ffddc3795e76933132
atborg <33327789+atborg@users.noreply.github.com>
2024-01-09 20:00:34 -0500

    pagination with sets of pages of a defined length

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -1,3 +1,4 @@
+import { PAGE_SET_LENGTH } from "@/config/pagination";
 import { AskMePages } from "@/types";
 
 export default function Pagination({
@@ -7,21 +8,46 @@ export default function Pagination({
   pages: AskMePages;
   handlePage: (pageNumber: number) => void;
 }) {
+  // if page is the last page then assign lastPage to pages.self
+  const lastPage = pages.last ? pages.last : pages.self;
+
+  const pageNumberOffest =
+    Math.floor((pages.self - 1) / PAGE_SET_LENGTH) * PAGE_SET_LENGTH;
+  const offsetToLastPage = lastPage - pageNumberOffest;
+  const isFinalPagesSet = offsetToLastPage <= PAGE_SET_LENGTH;
+  const pagesInSet = isFinalPagesSet ? offsetToLastPage : PAGE_SET_LENGTH;
   return (
     <div className="join">
-      <button
-        onClick={() => handlePage(pages.previous!)}
-        className={`btn join-item ${!pages.first && "btn-disabled"}`}
-      >
-        «
-      </button>
-      <button className="btn join-item">{pages.self}</button>
-      <button
-        onClick={() => handlePage(pages.next || pages.last!)}
-        className={`btn join-item ${!pages.last && "btn-disabled"}`}
-      >
-        »
-      </button>
+      {pageNumberOffest != 0 && (
+        <button
+          onClick={() => handlePage(pageNumberOffest)}
+          className="btn join-item"
+        >
+          «
+        </button>
+      )}
+      {[...Array(pagesInSet)].map((_, i) => {
+        const pageNumber = i + 1 + pageNumberOffest;
+        return (
+          <button
+            key={i}
+            onClick={() => handlePage(pageNumber)}
+            className={`btn join-item ${
+              pages.self == pageNumber && "btn-active"
+            }`}
+          >
+            {pageNumber}
+          </button>
+        );
+      })}
+      {!isFinalPagesSet && (
+        <button
+          onClick={() => handlePage(pageNumberOffest + PAGE_SET_LENGTH + 1)}
+          className="btn join-item"
+        >
+          »
+        </button>
+      )}
     </div>
   );
 }
